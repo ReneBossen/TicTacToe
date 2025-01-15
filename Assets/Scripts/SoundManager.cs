c2b34d6d4c0ef34c91ead6c5af4fc4d697005534
ReneBossen <renebossen99@gmail.com>
2025-01-15 23:10:48 +0100

    GameSounds Added

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,3 +1,4 @@
+using Mirror;
 using System;
 using UnityEngine;
 
@@ -6,10 +7,34 @@ namespace Assets.Scripts
     public class SoundManager : MonoBehaviour
     {
         [SerializeField] private GameObject _placeSfxPrefab;
+        [SerializeField] private GameObject _winSfxPrefab;
+        [SerializeField] private GameObject _loseSfxPrefab;
 
         private void Start()
         {
             GameManager.Instance.OnPlacedObject += GameManager_OnPlacedObject;
+            GameManager.Instance.OnGameWin += GameManager_OnGameWin;
+        }
+
+        private void GameManager_OnGameWin(object sender, GameManager.OnGameWinEventArgs args)
+        {
+            NetworkIdentity localPlayerIdentity = NetworkClient.connection.identity;
+
+            if (localPlayerIdentity == null)
+                return;
+
+            PlayerController player = localPlayerIdentity.GetComponent<PlayerController>();
+
+            if (player.GetPlayerType() == args.winPlayerType)
+            {
+                GameObject sfxGameObject = Instantiate(_winSfxPrefab);
+                Destroy(sfxGameObject.gameObject, 3f);
+            }
+            else
+            {
+                GameObject sfxGameObject = Instantiate(_loseSfxPrefab);
+                Destroy(sfxGameObject.gameObject, 3f);
+            }
         }
 
         private void GameManager_OnPlacedObject(object sender, EventArgs args)
